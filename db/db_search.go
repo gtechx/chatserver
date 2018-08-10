10809ba1895e4aba3066421df045ec3ad2c386b4
wangyanqing <625259292@qq.com>
2018-08-11 00:07:38 +0800

    modify

--- db/db_search.go
+++ db/db_search.go
@@ -13,12 +13,18 @@ func (db *DBManager) SearchUserById(id uint64) (*AppDataPublicWithID, error) {
 
 func (db *DBManager) SearchUserByNickname(nickname string) ([]*AppDataPublicWithID, error) {
 	ret := []*AppDataPublicWithID{}
-	retdb := db.sql.Model(appdata_table).Where("nickname = ?", "%"+nickname+"%").Scan(ret)
+	retdb := db.sql.Model(appdata_table).Where("nickname like ?", "%"+nickname+"%").Scan(&ret)
+	if retdb.Error == gorm.ErrRecordNotFound {
+		return nil, nil
+	}
 	return ret, retdb.Error
 }
 
 func (db *DBManager) SearchRoom(roomname string) ([]*Room, error) {
 	ret := []*Room{}
-	retdb := db.sql.Model(room_table).Where("roomname = ?", "%"+roomname+"%").Scan(ret)
+	retdb := db.sql.Model(room_table).Where("roomname like ?", "%"+roomname+"%").Scan(&ret)
+	if retdb.Error == gorm.ErrRecordNotFound {
+		return nil, nil
+	}
 	return ret, retdb.Error
 }
